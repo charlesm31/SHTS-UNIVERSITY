0b78d9a9713460ea99a0544eb387bd2671e365b8
devJc <developer1031@gmail.com>
2016-12-02 20:32:17 +0800

    90% coded landing page for desktop view

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -4,13 +4,12 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
 
         <title>@yield('title')</title>
     </head>
     <body>
-       <div class="container-fluid">       
         @yield('content')
-       </div>
     </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,31 +6,225 @@
 
 @section('content')
 <header>
-
+  <div class="container">   
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">      
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="fa fa-phone"></span> 1234 567 890</a></li>
+        <li><a href="#"><span class="fa fa-envelope"></span> Contact Us</a></li>
+        <li><a href="#"><span class="fa fa-question-circle"></span> FAQs</a></li>
+        <li><a href="#"><span class="fa fa-user"></span> Student Login</a></li>   
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
 </header>
 
-<nav>
+<nav class="navbar navbar-default">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">SHTS University</a>
+    </div>
 
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">      
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Lorem</a></li>
+        <li><a href="#">Dolor</a></li>
+        <li><a href="#">Ipsum</a></li>
+        <li><a href="#">Amet</a></li>   
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
 </nav>
 
 <section id="banner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-7 col-md-4">
+                <form class="login-form">
+                    <h1>Student Login</h1>
 
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" class="form-control" name="username" id="username" placeholder="e.g.. john">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" name="password"  id="password" placeholder="e.g.. ***********">
+                    </div>
+
+                    <button type="submit" class="btn btn-lg btn-primary full-width" id="btn-login"> Sign In <i class="fa fa-arrow-circle-o-right"></i></button>
+                </form>
+            </div>
+        </div>
+    </div>
 </section>
 
 <section id="courses">
+    <div class="container">
+        <h2 class="section-header">Our Courses<h2>
 
+        <div class="row">
+            <div class="col-md-3">
+                <div class="content-wrapper">
+                    <h3>Information Technology</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Donec sollicitudin molestie malesuada. Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+                    <button class="btn btn-lg btn-primary">Learn more</button>
+                </div>
+            </div> 
+
+            <div class="col-md-3">
+                <div class="content-wrapper">
+                    <h3>Arts & Media</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Donec sollicitudin molestie malesuada. Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+                    <button class="btn btn-lg btn-primary">Learn more</button>
+                </div>
+            </div> 
+
+            <div class="col-md-3">
+                <div class="content-wrapper">
+                    <h3>Business Management</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Donec sollicitudin molestie malesuada. Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+                     <button class="btn btn-lg btn-primary">Learn more</button>
+                </div>
+            </div> 
+
+            <div class="col-md-3">
+                <div class="content-wrapper">
+                    <h3>Project Management</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin eget tortor risus. Donec sollicitudin molestie malesuada. Curabitur aliquet quam id dui posuere blandit. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+                     <button class="btn btn-lg btn-primary">Learn more</button>
+                </div>
+            </div>           
+        </div>
+    
+    </div>
 </section>
-
+    
 <section id="about">
+    <div class="container">
+        <h2 class="section-header">About Us<h2>
 
+        <div class="padded-content">
+            <p>Proin eget tortor risus. Pellentesque in ipsum id orci porta dapibus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus.</p>
+            <p>Nulla porttitor accumsan tincidunt. Donec sollicitudin molestie malesuada. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Donec rutrum congue leo eget malesuada. Quisque velit nisi, pretium ut lacinia in, elementum id enim.</p>
+        </div>
+     </div>
 </section>
 
 <section id="contact">
-
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5">
+                <h4>Stay Connected</h4>
+                <img src="{{ url('img\desktop\ftr-soc-fb.png') }}" alt="fb">
+                <img src="{{ url('img\desktop\ftr-soc-twt.png') }}" alt="fb">
+                <img src="{{ url('img\desktop\ftr-soc-in.png') }}" alt="fb">
+                <img src="{{ url('img\desktop\ftr-soc-gplus.png') }}" alt="fb">
+                <img src="{{ url('img\desktop\ftr-soc-pin.png') }}" alt="fb">
+            </div>
+            <div class="col-md-7">
+                <h4>Sign up to our newsletter</h4>
+                <form class="form-inline">
+                    <div class="form-group">
+                        <input type="text" class="form-control" id="name" name="contact_name" placeholder="Name">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" id="contact_email" name="contact_email" placeholder="Email">
+                    </div>                
+                    <button class="btn btn-warning">Sign up</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </section>
 
 <footer>
-        
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2 col-md-offset-1">
+                <h5>Lorem</h5>
+                <a href="#">Mauris vel imperdiet</a>
+                <a href="#">Faucibus metus</a>
+                <a href="#">Nulla sed quam dictum</a>
+                <a href="#">Ultricies sit amet</a>
+                <a href="#">Sed tempus</a>
+                <a href="#">Neque non tincidunt</a>
+                <a href="#">Vivamus elementum</a>
+                <a href="#">Mit ut semper</a>
+            </div>
+
+            <div class="col-md-2">
+                <h5>Ipsum</h5>
+                <a href="#">Mauris vel imperdiet</a>
+                <a href="#">Faucibus metus</a>
+                <a href="#">Nulla sed quam dictum</a>
+                <a href="#">Ultricies sit amet</a>
+                <a href="#">Sed tempus</a>
+                <a href="#">Neque non tincidunt</a>
+                <a href="#">Vivamus elementum</a>
+            </div>
+
+            <div class="col-md-2">
+                <h5>Dolor</h5>
+                <a href="#">Mauris vel imperdiet</a>
+                <a href="#">Faucibus metus</a>
+                <a href="#">Nulla sed quam dictum</a>
+                <a href="#">Ultricies sit amet</a>
+                <a href="#">Sed tempus</a>
+                <a href="#">Neque non tincidunt</a>
+                <a href="#">Vivamus elementum</a>
+                <a href="#">Mit ut semper</a>
+                <a href="#">Portitur vitae nulla dolor</a>
+                <a href="#">Sapien mollis</a>
+            </div>
+
+            <div class="col-md-2">
+                <h5>Sit amet</h5>
+                <a href="#">Mauris vel imperdiet</a>
+                <a href="#">Faucibus metus</a>
+                <a href="#">Nulla sed quam dictum</a>
+                <a href="#">Ultricies sit amet</a>
+                <a href="#">Sed tempus</a>
+                <a href="#">Neque non tincidunt</a>
+                <a href="#">Vivamus elementum</a>
+                <a href="#">Portitur vitae nulla dolor</a>
+                <a href="#">Mit ut semper</a>
+            </div>
+
+            <div class="col-md-2">
+                <h5>Consectetur</h5>
+                <a href="#">Mauris vel imperdiet</a>
+                <a href="#">Faucibus metus</a>
+                <a href="#">Nulla sed quam dictum</a>
+                <a href="#">Ultricies sit amet</a>
+                <a href="#">Sed tempus</a>
+                <a href="#">Neque non tincidunt</a>
+                <a href="#">Vivamus elementum</a>
+                <a href="#">Mit ut semper</a>
+            </div>
+        </div>
+
+        <hr>
+        <div id="copyright">
+            <p>©Copyright 2015 SHTS University, All rights reserved</p>
+            <ul>
+                <li>Lorem</li>
+                <li>Ipsum</li>
+                <li>Dolor</li>
+                <li>Sit</li>
+                <li>Amet</li>
+            </ul>
+        </div> 
+    </div>
 </footer>
 @endsection
 

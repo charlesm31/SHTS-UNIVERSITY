89dee19588fcfcbddc018cd2c9ce7e7dabe37703
devJc <developer1031@gmail.com>
2016-12-02 23:04:09 +0800

    WIP: Patch for mobile responsive design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -169,65 +169,6 @@
 
 <footer>
     <div class="container">
-    <div class="panel-group visible-xs visible-sm" id="accordion" role="tablist" aria-multiselectable="true">
-        <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="headingOne">
-            <h4 class="panel-title">
-                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                Lorem
-                </a>
-            </h4>
-            </div>
-            <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-            <div class="panel-body">
-                <a href="#">Mauris vel imperdiet</a>
-                <a href="#">Faucibus metus</a>
-                <a href="#">Nulla sed quam dictum</a>
-                <a href="#">Ultricies sit amet</a>
-                <a href="#">Sed tempus</a>
-                <a href="#">Neque non tincidunt</a>
-                <a href="#">Vivamus elementum</a>
-                <a href="#">Mit ut semper</a>
-            </div>
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="headingTwo">
-            <h4 class="panel-title">
-                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Ipsum
-                </a>
-            </h4>
-            </div>
-            <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-            <div class="panel-body">
-                <a href="#">Mauris vel imperdiet</a>
-                <a href="#">Faucibus metus</a>
-                <a href="#">Nulla sed quam dictum</a>
-                <a href="#">Ultricies sit amet</a>
-                <a href="#">Sed tempus</a>
-                <a href="#">Neque non tincidunt</a>
-                <a href="#">Vivamus elementum</a>
-                <a href="#">Mit ut semper</a>
-            </div>
-            </div>
-        </div>
-        <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="headingThree">
-            <h4 class="panel-title">
-                <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Collapsible Group Item #3
-                </a>
-            </h4>
-            </div>
-            <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-            <div class="panel-body">
-                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-            </div>
-            </div>
-        </div>
-    </div>
-
         <div class="row hidden-xs hidden-sm">
             <div class="col-md-2 col-md-offset-1">
                 <h5>Lorem</h5>
@@ -291,6 +232,78 @@
                 <a href="#">Mit ut semper</a>
             </div>
         </div>
+
+        <div class="panel-group visible-xs visible-sm" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingOne">
+                    <h4 class="panel-title">
+                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Lorem
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                    <div class="panel-body">
+                    <ul class="list-group">
+                            <li class="list-group-item"><a href="#">Mauris vel imperdiet</a></li>
+                            <li class="list-group-item"><a href="#">Faucibus metus</a></li>
+                            <li class="list-group-item"><a href="#">Nulla sed quam dictum</a></li>
+                            <li class="list-group-item"><a href="#">Ultricies sit amet</a></li>
+                            <li class="list-group-item"><a href="#">Sed tempus</a></li>
+                            <li class="list-group-item"><a href="#">Neque non tincidunt</a></li>
+                            <li class="list-group-item"><a href="#">Vivamus elementum</a></li>
+                            <li class="list-group-item"><a href="#">Mit ut semper</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingTwo">
+                    <h4 class="panel-title">
+                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Ipsum
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                    <div class="panel-body">
+                        <ul class="list-group">
+                            <li class="list-group-item"><a href="#">Mauris vel imperdiet</a></li>
+                            <li class="list-group-item"><a href="#">Faucibus metus</a></li>
+                            <li class="list-group-item"><a href="#">Nulla sed quam dictum</a></li>
+                            <li class="list-group-item"><a href="#">Ultricies sit amet</a></li>
+                            <li class="list-group-item"><a href="#">Sed tempus</a></li>
+                            <li class="list-group-item"><a href="#">Neque non tincidunt</a></li>
+                            <li class="list-group-item"><a href="#">Vivamus elementum</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingThree">
+                    <h4 class="panel-title">
+                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Dolor
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                    <div class="panel-body">
+                        <ul class="list-group">
+                            <li class="list-group-item"><a href="#">Mauris vel imperdiet</a></li>
+                            <li class="list-group-item"><a href="#">Faucibus metus</a></li>
+                            <li class="list-group-item"><a href="#">Nulla sed quam dictum</a></li>
+                            <li class="list-group-item"><a href="#">Ultricies sit amet</a></li>
+                            <li class="list-group-item"><a href="#">Sed tempus</a></li>
+                            <li class="list-group-item"><a href="#">Neque non tincidunt</a></li>
+                            <li class="list-group-item"><a href="#">Vivamus elementum</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>        
 
         <hr>
         <div id="copyright">

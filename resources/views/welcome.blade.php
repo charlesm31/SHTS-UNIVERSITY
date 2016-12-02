22b1c6cb7bc19b5a3ad97ed2226afcf0d09b1756
devJc <developer1031@gmail.com>
2016-12-02 17:30:29 +0800

    INITIAL TEMPLATE

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+@extends('layout.master')
+
+@section('title') 
+    Home - SHTS University
+@stop
+
+@section('content')
+<header>
+
+</header>
+
+<nav>
+
+</nav>
+
+<section id="banner">
+
+</section>
+
+<section id="courses">
+
+</section>
+
+<section id="about">
+
+</section>
+
+<section id="contact">
+
+</section>
+
+<footer>
+        
+</footer>
+@endsection
+

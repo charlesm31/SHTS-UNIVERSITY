1f6f5b63137659afa3b77db4aab6dd06b3889779
JC Mallari <JC Mallari>
2016-12-05 09:18:16 +0800

    Migration and Seeder for User

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'id' => 1,
+            'type' => 'admin',
+            'username' => 'admin',
+            'password' => bcrypt('admin123!'),
+            'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+            'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+        ]);
+
+        DB::table('users')->insert([
+            'id' => 2,
+            'type' => 'user',
+            'username' => 'user',
+            'password' => bcrypt('user123!'),
+            'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
+            'updated_at' => \Carbon\Carbon::now()->toDateTimeString(),
+        ]);
+    }
+}
